b81d67e6be68a613f7740e5eec671211eac2f4a1
xles <xles@mirakulix.org>
2014-01-11 21:46:39 +0100

    Implemented getopt for option parsing.

--- journal.c
+++ journal.c
@@ -4,44 +4,57 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
 #include "colour.h"
 #include "journal.h"
 #include "mongoose.h"
 
 #define SERVE_ROOT "."
 #define SERVE_PORT "4000"
+#define OPTIONS "asdf"
+
 
 int main(int argc, char *argv[])
 {
 	if (argc == 1) {
-		printf("Basic usage: journal [command]\n");
+		printf("Basic usage: journal <options> [command]\n");
 		exit(1);
-	} else {
-		switch ((int) argv[1][0]) {
-			case '-':
-				parse_options(argv[1]);
+	}
+
+	int c = getopt(argc, argv, OPTIONS);
+	while (1) {
+		if (c == -1)
+			break;
+
+		switch (c) {
+			case 'f':
+				printf("f\n");
 				break;
-			default:
-				printf("Sorry, no such option.\n");
+			case '?':
+				printf("dafuq?\n");
 				break;
 		}
 	}
 
-}
-
-/*
-				serve();
+	if (!strcmp(argv[optind],"serve")) {
+		serve();
+	} else if (!strcmp(argv[optind],"editor")) {
 		char * editor;
 		editor = getenv("EDITOR");
 		printf("Your default editor is: '%s%s%s'\n", 
 			ANSI_COLOR_MAGENTA, 
 			editor, 
-			ANSI_COLOR_RESET);
-*/
+			ANSI_COLOR_RESET);		
+	} else {
+		printf("Invalid operand\n");
+	}
+}
 
 void parse_options(char *options)
 {
-	printf("%s\n", options);
+	printf("options: %s\n", options);
 }
 
 void serve(void)
@@ -52,7 +65,7 @@ void serve(void)
 	mg_set_option(server, "document_root", SERVE_ROOT);
 	mg_set_option(server, "listening_port", SERVE_PORT);
 	
-	while (true) {
+	while (1) {
 		mg_poll_server(server, 1000);
 	} 
 	
